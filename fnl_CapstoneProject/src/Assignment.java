4f81d76d89b8f1f77f73689af6f5f781e81ce11a
Ishanvi Deodhar <ideodhar431@student.fuhsd.org>
2025-05-09 11:57:15 -0700

    Update Assignment.java

--- fnl_CapstoneProject/src/Assignment.java
+++ fnl_CapstoneProject/src/Assignment.java
@@ -9,9 +9,41 @@ public class Assignment extends Event  {
 	private int progress;
 
 	
-	public Assignment(String title, String description, Date date, String course, Priority priority) {
+	public Assignment(String title, String description, Date date, String course, Priority priority, int progress) {
 		super(title, description, date);
 		this.course = course;
 		this.priority = priority;
+		this.progress = progress;
+	}
+	
+	public String getCourse() 
+	{
+		return course;
+	}
+	
+	public String setCourse(String course)
+	{
+		return this.course = course;
+	}
+	
+	public Priority getPriority()
+	{
+		return priority;
+		
+	}
+	
+	public Priority etPriority(Priority priority)
+	{
+		return this.priority = priority;
+	}
+	
+	public int getProgress()
+	{
+		return progress;
+	}
+	
+	public int setProgress()
+	{
+		return this.progress = progress;
 	}
 }
