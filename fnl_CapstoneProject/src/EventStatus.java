1fe0b1746d66467f72f5912d17ead7c70d92c1e1
Ishanvi Deodhar <ideodhar431@student.fuhsd.org>
2025-05-09 12:29:19 -0700

    rev 2.5

--- fnl_CapstoneProject/src/EventStatus.java
+++ fnl_CapstoneProject/src/EventStatus.java
@@ -1,5 +1,25 @@
 
 public enum EventStatus {
-	PENDING, IN_PROGRESS, COMPLETED, CANCELLED;
+	PENDING, 
+	IN_PROGRESS, 
+	COMPLETED, 
+	CANCELLED;
+	
+	public String toString()
+	{
+		switch (this) {
+        case PENDING:
+        	return "Pending";
+        case IN_PROGRESS:
+        	return "In Progress";
+        case COMPLETED:
+        	return "Completed";
+        case CANCELLED:
+        	return "Cancelled";
+        default:
+            return "Pending";
+		}
+	}
+	
 
 }
